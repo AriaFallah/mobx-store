e4098fbd3340b77a6724432b1354fb1c312236ad
AriaFallah <aria@fallah.us>
2016-04-19 18:45:33 -0400

    :art: remove private method calls

--- src/index.js
+++ src/index.js
@@ -2,70 +2,75 @@
 
 import _ from 'lodash'
 import { observable } from 'mobx'
-import memoize from 'sb-memoize'
 
 const dbObject = {}
 function db(key: string): Array<any> {
   // If the observable array doesn't exist create it
   if (!dbObject[key]) {
+    const obs = observable([])
     dbObject[key] = {
-      data: observable([]), // The actual data
-      __chksum: JSON.stringify([]) // Used to diff the array
+      obs, // The observable array
+      __data: loWrap(obs.slice(), key) // The underlying array
     }
   }
-  return loWrap(dbObject[key].data.$mobx.values, key)
+  return dbObject[key].__data
 }
 
-// Wrap the collection such that you can call lodash methods on it implicitly.
+// Wrap a collection such that you can call lodash methods on it implicitly.
 // Normally you would need to do something like _.method(collection), but now
 // you can just do collection.method().
 function loWrap(array: Array<any>, key: string) {
+  const lodash = _.runInContext()
   const wrapped = _.chain(array)
+  const updateObs = _.partialRight(update, key)
 
-  // Make it so every method called in the chain calls value() on itself right after.
-  // This way it doesn't really behave like a chain, but allows the collection.method() usage
+  // Make it so every method called in the chain calls .value() on itself right after.
+  // This way it doesn't really behave like a chain, but allows the collection.method() API.
   for (const method of _.functionsIn(wrapped)) {
-    wrapped[method] = _.flow(wrapped[method], unwrap, _.partialRight(reportChange, key))
+    wrapped[method] = _.flow(
+      wrapped[method],
+      unwrap,
+      updateObs
+    )
   }
 
-  // We need to fix chain so it'll actually chain.
-  // We just broke it above by calling .value() after it's called.
+  // Expose the observable array
+  wrapped.obs = function() {
+    return dbObject[key].obs
+  }
+
+  // Fix the chain function so it doesn't do .chain().value(), which breaks chaining.
   wrapped.chain = function() {
     // Alter the prototype of lodash ONLY for this chain
-    // such that a change is reported when .value() is called at the end of the chain
-    const lodash = _.runInContext()
-    lodash.prototype.value = lodash.flow(lodash.prototype.value, lodash.partialRight(reportChange, key))
-    return lodash.chain(dbObject[key].data.$mobx.values)
+    // to update the observable after value() is called
+    lodash.prototype.value =
+      lodash.flow(
+        lodash.prototype.value,
+        updateObs
+      )
+
+    // Return a chain with an altered value prototype
+    return lodash.chain(array)
   }
 
-  // Don't make .value() jump through the hoops above. Just return the value.
-  wrapped.value = function() {
-    return dbObject[key].data
-  }
-
+  // Fix the value function so it doesn't do .value().value().
+  wrapped.value = _.prototype.value
   return wrapped
 }
 
-// Unwrap the result of a lodash function call if it is wrapped
-function unwrap(value: any): any {
-  if (!value) return null
-  return _.isFunction(value.value) ? value.value() : value
+// Unwrap the result of a lodash function chain
+function unwrap(chain: any): any {
+  return !chain ? null : chain.value ? chain.value() : chain
 }
 
-// Report a change if the collection has changed
-function reportChange(value: any, key: string) {
-  const chksum = JSON.stringify(dbObject[key].data)
-  if (chksum !== dbObject[key].__chksum) {
-    dbObject[key].__chksum = chksum
-    dbObject[key].data.$mobx.atom.reportChanged()
-  } else {
-    dbObject[key].data.$mobx.atom.reportObserved()
+// Make it so changes to the underlying array reflect in the observable array
+function update(value: any, key: string) {
+  const data = dbObject[key].__data.value()
+  if (JSON.stringify(dbObject[key].obs.slice()) !== JSON.stringify(data)) {
+    dbObject[key].obs.replace(_.cloneDeep(data))
   }
   return value
 }
 
-// Memoize the db function and expose the object
-const memoized = memoize(db)
-memoized.object = dbObject
-
-export default memoized
+export default db
+db.object = dbObject // expose the dbObject
