aa0d12962ba6fd4fc849676d613c9c4bbc650cd9
AriaFallah <aria@fallah.us>
2016-04-19 14:22:15 -0400

    :art: memoize the db function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@
 
 import _ from 'lodash'
 import { observable } from 'mobx'
+import memoize from 'sb-memoize'
 
-export default function db(key: string): Array<any> {
+const dbObject = {}
+function db(key: string): Array<any> {
   // If the observable array doesn't exist create it
-  if (!db.object[key]) {
-    db.object[key] = {
+  if (!dbObject[key]) {
+    dbObject[key] = {
       data: observable([]), // The actual data
       __chksum: JSON.stringify([]) // Used to diff the array
     }
   }
-  return loWrap(db.object[key].data.$mobx.values, key)
+  return loWrap(dbObject[key].data.$mobx.values, key)
 }
-db.object = {}
 
 // Wrap the collection such that you can call lodash methods on it implicitly.
 // Normally you would need to do something like _.method(collection), but now
@@ -34,12 +35,12 @@ function loWrap(array: Array<any>, key: string) {
     // such that a change is reported when .value() is called at the end of the chain
     const lodash = _.runInContext()
     lodash.prototype.value = lodash.flow(lodash.prototype.value, lodash.partialRight(reportChange, key))
-    return lodash.chain(db.object[key].data.$mobx.values)
+    return lodash.chain(dbObject[key].data.$mobx.values)
   }
 
   // Don't make .value() jump through the hoops above. Just return the value.
   wrapped.value = function() {
-    return db.object[key].data
+    return dbObject[key].data
   }
 
   return wrapped
@@ -53,12 +54,18 @@ function unwrap(value: any): any {
 
 // Report a change if the collection has changed
 function reportChange(value: any, key: string) {
-  const chksum = JSON.stringify(db.object[key].data)
-  if (chksum !== db.object[key].__chksum) {
-    db.object[key].__chksum = chksum
-    db.object[key].data.$mobx.atom.reportChanged()
+  const chksum = JSON.stringify(dbObject[key].data)
+  if (chksum !== dbObject[key].__chksum) {
+    dbObject[key].__chksum = chksum
+    dbObject[key].data.$mobx.atom.reportChanged()
   } else {
-    db.object[key].data.$mobx.atom.reportObserved()
+    dbObject[key].data.$mobx.atom.reportObserved()
   }
   return value
 }
+
+// Memoize the db function and expose the object
+const memoized = memoize(db)
+memoized.object = dbObject
+
+export default memoized
