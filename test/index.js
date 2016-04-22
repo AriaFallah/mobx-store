58c9c5c5e5af20bf3ad9add064b4c01fcc25959c
AriaFallah <aria@fallah.us>
2016-04-22 01:32:01 -0400

    :art: get coverage to 100%

--- test/index.js
+++ test/index.js
@@ -14,13 +14,20 @@ global.localStorage = {
 const db = store()
 const local = store('db', { storage })
 
-test('It reads and writes to local storage', function(t) {
+test('Local storage works', function(t) {
+  storage.read('x')
+  t.deepEqual(JSON.parse(global.localStorage.store.x), {})
+  storage.write('x', { hello: 'world' })
+  t.deepEqual(JSON.parse(global.localStorage.store.x), { hello: 'world' })
+})
+
+test('Store reads from and writes to local storage', function(t) {
   t.deepEqual(local('a').value(), [1, 2, 3])
-  local('a').assign([4, 5 ,6])
+  local('a').assign([4, 5, 6])
   t.deepEqual(JSON.parse(global.localStorage.store.db).a.__data, [4, 5, 6])
 })
 
-test('It works when calling a single method', function(t) {
+test('Store works when calling a single method', function(t) {
   let i = 0
   db('a').assign([1, 2, 3])
   autorun(() => i += noop(db('a').value()[0]))
@@ -28,7 +35,7 @@ test('It works when calling a single method', function(t) {
   t.is(i, 2)
 })
 
-test('It should not report a change when nothing changes', function(t) {
+test('Store should not report a change when nothing changes', function(t) {
   let i = 0
   db('b').assign([1, 2, 3])
   autorun(() => i += noop(db('b').value()[0]))
@@ -36,7 +43,7 @@ test('It should not report a change when nothing changes', function(t) {
   t.not(i, 2)
 })
 
-test('It works when chaining', function(t) {
+test('Store works when chaining', function(t) {
   let i = 0
   db('c').assign([{ a: 1 }, { a: 2 }, { a: 3 }])
   autorun(() => i += noop(db('c').value()[0]))
