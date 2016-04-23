0bb892801bad99bc05eea64fa51eb53e8532037f
AriaFallah <aria@fallah.us>
2016-04-23 16:00:01 -0400

    :art: make localstorage compatible with new API

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,24 +1,12 @@
 // @flow
 
-import { forOwn } from 'lodash'
-import { map as obsMap } from 'mobx'
-
-function deserialize(objString: string): Object {
-  const db = obsMap({})
-  const obj = JSON.parse(objString)
-
-  // Read the object, creating data in the db for each key
-  forOwn(obj, (value, key) => db.set(key, value))
-  return db
-}
-
 function read(source: string) {
   const data = localStorage.getItem(source)
   if (data) {
-    return deserialize(data)
+    return JSON.parse(data)
   }
   localStorage.setItem(source, '{}')
-  return obsMap({})
+  return {}
 }
 
 function write(dest: string, obj: Object) {
