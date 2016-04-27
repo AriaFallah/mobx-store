7f770553a0312944bef8c69cf27407553401459b
AriaFallah <aria@fallah.us>
2016-04-26 21:24:26 -0400

    :new: types for configuration and changes

--- src/types.js
+++ src/types.js
@@ -1,1 +1,14 @@
 // @flow
+
+export type Change = {
+  name?: number,
+  type: string,
+  object: Object,
+  index?: number,
+  addedCount?: number,
+  removed?: Array<any>
+}
+
+export type StoreConfig = {
+  historyLimit?: number
+}
